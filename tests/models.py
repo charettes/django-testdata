09eb76a1e47db860b82e6c9388ea7eef3dd713c2
Simon Charette <simon.charette@zapier.com>
2019-10-04 22:06:44 +0200

    Adjust Django version support.

--- tests/models.py
+++ tests/models.py
@@ -1,7 +1,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
+
+try:
+    from django.utils.encoding import python_2_unicode_compatible
+except ImportError:
+    def python_2_unicode_compatible(cls):
+        return cls
 
 
 @python_2_unicode_compatible
