6b66450c0451362ef3d4199bbb28598c5d369e8c
Adam Johnson <me@adamj.eu>
2021-04-16 17:20:31 +0100

    Push <3.2 skip into setUpClass because Python 2.7 doesn't support it at module level

--- tests/test_django_32.py
+++ tests/test_django_32.py
@@ -8,11 +8,13 @@ from django.test import SimpleTestCase
 
 from testdata import wrap_testdata
 
-if django.VERSION < (3, 2):
-    raise SkipTest("Django 3.2 only tests.")
-
 
 class WrapTestDataTests(SimpleTestCase):
+    @classmethod
+    def setUpClass(cls):
+        if django.VERSION < (3, 2):
+            raise SkipTest("Django 3.2 only test.")
+
     def test_raises_exception_on_use(self):
         msg = (
             "django-testdata should not be used on Django 3.2, since it has "
