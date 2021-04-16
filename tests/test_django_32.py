49682cb705d6f99c1b3b8e98b3822dc9487c46ff
Adam Johnson <me@adamj.eu>
2021-04-16 17:43:51 +0100

    Move skip to decorator since raising SkipTest in setUpClass crashes django

--- tests/test_django_32.py
+++ tests/test_django_32.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from unittest import SkipTest
+from unittest import skipIf
 
 import django
 from django.test import SimpleTestCase
@@ -9,12 +9,8 @@ from django.test import SimpleTestCase
 from testdata import wrap_testdata
 
 
+@skipIf(django.VERSION < (3, 2), "Django 3.2 only tests.")
 class WrapTestDataTests(SimpleTestCase):
-    @classmethod
-    def setUpClass(cls):
-        if django.VERSION < (3, 2):
-            raise SkipTest("Django 3.2 only test.")
-
     def test_raises_exception_on_use(self):
         msg = (
             "django-testdata should not be used on Django 3.2, since it has "
