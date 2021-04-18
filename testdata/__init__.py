1a524b39dbfc2a06eebc10b945795dfd681592f8
Simon Charette <simon.charette@zapier.com>
2021-04-17 22:11:27 -0400

    Release 1.0.3 to bundle up the latest adjustments.

--- testdata/__init__.py
+++ testdata/__init__.py
@@ -5,6 +5,6 @@ import django.utils.version
 
 __all__ = ['VERSION', '__version__', 'testdata', 'wrap_testdata']
 
-VERSION = (1, 0, 2, 'final', 0)
+VERSION = (1, 0, 3, 'final', 0)
 
 __version__ = django.utils.version.get_version(VERSION)
